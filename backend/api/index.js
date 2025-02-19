4765e6fcf326bd177ff41f1564f7177c151a7583
Wahyu <wabredz1234@gmail.com>
2025-02-19 13:48:38 +0700

    add cors for localhost:5173

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import NewRoute from "./routes/new-route.js"
 
 const app = express();
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173",],
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 
