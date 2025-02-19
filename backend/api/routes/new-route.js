b92c49648a504fe96ca48d3d70ffb3d1f44669b4
Wahyu <wabredz1234@gmail.com>
2025-02-19 14:22:42 +0700

    add route get news by id

--- backend/api/routes/new-route.js
+++ backend/api/routes/new-route.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { getAllNew } from "../controllers/new-controller.js";
+import { getAllNew, getNewById } from "../controllers/new-controller.js";
 
 const router = express.Router();
 
 router.get("/get-all", getAllNew);
+router.get("/get-by-id/:id", getNewById);
 
 export default router;
