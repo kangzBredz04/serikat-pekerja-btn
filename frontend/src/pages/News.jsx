f988624664653b26d665a55c257229d1c16760a7
Wahyu <wabredz1234@gmail.com>
2025-02-19 13:50:00 +0700

    change data retrieval from regular variables to from api

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,68 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FaChevronLeft, FaChevronRight, FaArrowRight } from "react-icons/fa";
-
-const newsItems = [
-  {
-    title: "Silver Run SP-BTN x BTN Runners Community (HUT ke-25 SP-BTN)",
-    category: "Event",
-    image:
-      "https://btn.co.id/-/media/Images/Corporate/Promotion/2025/Jan/Awareness-BTN-Run-2025-843x474.png?h=474&w=843&hash=500B4D1D44C8E68E71406CDD8607F4E5",
-  },
-  {
-    title: "Video Content Competition (HUT ke-25 SP-BTN)",
-    category: "Competition",
-    image:
-      "https://www.btn.co.id/-/media/Images/Corporate/Promotion/2025/Feb/main-banner.png?h=474&w=843&hash=8123CDB2667542DAB35E29116FD23E55",
-  },
-  {
-    title: "Essay Competition (HUT ke-25 SP-BTN)",
-    category: "Competition",
-    image: "",
-  },
-  {
-    title: "HUT ke-25 SP-BTN: Kolaboratif, Sehat, dan Produktif",
-    category: "Anniversary",
-    image: "",
-  },
-  {
-    title:
-      "Ketahui dan Pahami, Ini Hak-hak Pekerja Perempuan yang Dijamin Negara",
-    category: "Education",
-    image:
-      "https://www.btn.co.id/-/media/Images/Corporate/Promotion/2025/Jan/HUT-CB-75-Banner-LP-843x474.png?h=474&w=843&hash=27A85271C982A572B16C4216FAA4BAAF",
-  },
-  {
-    title: "Role Pengurus SP-BTN",
-    category: "Organization",
-    image: "",
-  },
-  {
-    title: "Silver Run SP-BTN x BTN Runners Community (HUT ke-25 SP-BTN)",
-    category: "Event",
-    image:
-      "https://www.btn.co.id/-/media/Images/Corporate/Promotion/2025/Feb/bale-festival-banner-festive-copy.jpg",
-  },
-  {
-    title: "Video Content Competition (HUT ke-25 SP-BTN)",
-    category: "Competition",
-    image:
-      "https://btn.co.id/-/media/Images/Corporate/Promotion/2025/Feb/balepont-makassar.png?h=356&w=632&hash=F5FF9D2F022F053BEEAD68EB22F313E9",
-  },
-  {
-    title: "Essay Competition (HUT ke-25 SP-BTN)",
-    category: "Competition",
-    image: "",
-  },
-];
+import { AllContext } from "../App";
 
 const News = () => {
+  const { news } = useContext(AllContext);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6;
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = newsItems.slice(indexOfFirstItem, indexOfLastItem);
-  const totalPages = Math.ceil(newsItems.length / itemsPerPage);
+  const currentItems = news.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(news.length / itemsPerPage);
+
+  console.log(news);
 
   return (
     <div className="max-w-7xl mx-auto py-10 px-4 text-center">
@@ -72,9 +22,9 @@ const News = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {currentItems.map((item, index) => (
           <div key={index} className="bg-white p-4 rounded-lg shadow-lg h-96">
-            {item.image ? (
+            {item.image_url ? (
               <img
-                src={item.image}
+                src={item.image_url}
                 alt={item.title}
                 className="h-48 w-full object-cover rounded-t-lg"
               />
