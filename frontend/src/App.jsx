97f6f4fe213d2e2a0ba945ab5800325c22cf1432
Wahyu <wabredz1234@gmail.com>
2025-02-19 13:49:23 +0700

    create context and get news data from api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { createContext, useEffect, useState } from "react";
+import { api } from "./utils";
+
+export const AllContext = createContext();
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    api.get("/new/get-all").then((response) => setNews(response));
+  }, []);
+
+  if (news.length !== 0) {
+    console.log(news);
+  }
+
   return (
-    <div>
+    <AllContext.Provider
+      value={{
+        news,
+      }}
+    >
       <Header />
       <Outlet />
       <Footer />
-    </div>
+    </AllContext.Provider>
   );
 }
 
