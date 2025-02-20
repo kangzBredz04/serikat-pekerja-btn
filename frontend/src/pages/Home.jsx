1d0402926a76ca9fa15623c0bbef84d9b5586fd5
Wahyu <wabredz1234@gmail.com>
2025-02-20 09:24:02 +0700

    update color on Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
 
   return (
     <div>
-      <div className="min-h-screen w-full bg-gradient-to-b from-white to-blue-200 py-10 px-4   md:py-20 md:px-28">
+      <div className="min-h-screen w-full py-10 px-4 bg-gray-100 md:py-20 md:px-28">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="h-full w-full  flex-1">
             <img
@@ -50,20 +50,27 @@ function Home() {
               </p>
             </div>
             <Link
-              to={
-                "https://forms.zohopublic.com/dppspbtn/form/FormulirAnggotaSPBTN1/formperma/4KkNx7Qb8ZsR-E61vKSZ2fhM_0vOF4DRcsNRDFTvzOU"
-              }
+              // to={
+              //   "https://forms.zohopublic.com/dppspbtn/form/FormulirAnggotaSPBTN1/formperma/4KkNx7Qb8ZsR-E61vKSZ2fhM_0vOF4DRcsNRDFTvzOU"
+              // }
+              onClick={(e) => {
+                e.preventDefault();
+                window.open(
+                  "https://forms.zohopublic.com/dppspbtn/form/FormulirAnggotaSPBTN1/formperma/4KkNx7Qb8ZsR-E61vKSZ2fhM_0vOF4DRcsNRDFTvzOU",
+                  "_blank"
+                );
+              }}
             >
-              <button className="bg-blue-600 text-white px-4 py-2 md:px-6 md:py-2 rounded-full font-semibold shadow-md hover:bg-blue-800 cursor-pointer">
+              <button className="bg-red-600 text-white px-4 py-2 md:px-6 md:py-2 rounded-full font-semibold shadow-md hover:bg-red-800 cursor-pointer">
                 Daftar Anggota
               </button>
             </Link>
           </div>
         </div>
       </div>
-      <div className="min-h-screen bg-gradient-to-t from-white to-blue-200 py-10 md:py-16 px-5 md:px-8">
-        <div className="max-w-3xl md:max-w-5xl mx-auto text-center">
-          <h2 className="text-2xl md:text-3xl font-extrabold text-blue-900">
+      <div className="min-h-screen py-10 md:py-16 px-5 md:px-8 flex flex-col items-center justify-center bg-gray-100">
+        <div className="max-w-3xl md:max-w-5xl text-center">
+          <h2 className="text-2xl md:text-3xl font-extrabold text-red-900">
             VISI KAMI
           </h2>
           <p className="text-gray-600 mt-2 text-base md:text-lg">
@@ -71,27 +78,24 @@ function Home() {
             untuk Mewujudkan Kesejahteraan Pekerja yang Berimbang.
           </p>
         </div>
-
-        <div className="mt-8 md:mt-10 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-3xl md:max-w-5xl mx-auto">
-          <div className="bg-white shadow-lg rounded-xl p-4 md:p-6">
-            <h3 className="text-lg font-bold text-blue-900">Independen</h3>
+        <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-3xl md:max-w-5xl mx-auto">
+          <div className="bg-white/80 shadow-2xl backdrop-blur-md rounded-xl p-6 transform transition-all duration-300 hover:scale-105">
+            <h3 className="text-lg font-bold text-red-900">Independen</h3>
             <p className="text-gray-700 mt-2 leading-relaxed text-sm md:text-base">
               SPBTN tidak terafiliasi dengan kepentingan pihak tertentu dan
               memperjuangkan kepentingan pekerja secara merdeka untuk
               keberlanjutan kejayaan BTN dan peningkatan kesejahteraan Pekerja.
             </p>
           </div>
-
-          <div className="bg-white shadow-lg rounded-xl p-4 md:p-6">
-            <h3 className="text-lg font-bold text-blue-900">Representatif</h3>
+          <div className="bg-white/80 shadow-2xl backdrop-blur-md rounded-xl p-6 transform transition-all duration-300 hover:scale-105">
+            <h3 className="text-lg font-bold text-red-900">Representatif</h3>
             <p className="text-gray-700 mt-2 leading-relaxed text-sm md:text-base">
               SPBTN sebagai lembaga formal yang representatif menyalurkan
               aspirasi seluruh pekerja BTN.
             </p>
           </div>
-
-          <div className="bg-white shadow-lg rounded-xl p-4 md:p-6">
-            <h3 className="text-lg font-bold text-blue-900">Dinamis</h3>
+          <div className="bg-white/80 shadow-2xl backdrop-blur-md rounded-xl p-6 transform transition-all duration-300 hover:scale-105">
+            <h3 className="text-lg font-bold text-red-900">Dinamis</h3>
             <p className="text-gray-700 mt-2 leading-relaxed text-sm md:text-base">
               Organisasi SPBTN dapat bergerak secara dinamis dan agile dalam
               menjalankan roda organisasinya untuk mencapai tujuan SPBTN.
@@ -99,8 +103,9 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="min-h-full bg-gradient-to-b from-white to-blue-200 py-10 px-6">
-        <h2 className="text-3xl font-bold text-blue-600 mb-6">News</h2>
+
+      <div className="min-h-full py-10 px-6 bg-gray-100">
+        <h2 className="text-3xl font-bold text-red-600 mb-6">News</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {loading
             ? // Skeleton Loader
@@ -145,7 +150,7 @@ function Home() {
                         "id_news"
                       )}`;
                     }}
-                    className="flex items-center text-blue-600 mt-4 font-medium cursor-pointer"
+                    className="flex items-center text-red-600 mt-4 font-medium cursor-pointer"
                   >
                     Read More <FaArrowRight className="ml-2" />
                   </a>
@@ -153,7 +158,7 @@ function Home() {
               ))}
         </div>
         <div className="text-center mt-6">
-          <a className="text-blue-700 font-semibold text-lg" href="/news">
+          <a className="text-red-700 font-semibold text-lg" href="/news">
             View All
           </a>
         </div>
