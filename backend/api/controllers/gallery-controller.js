053eed3b5e9ab029981686a3b720fc7597021eff
Wahyu <wabredz1234@gmail.com>
2025-02-27 11:46:24 +0700

    add upload or add image in gallery controller

--- backend/api/controllers/gallery-controller.js
+++ backend/api/controllers/gallery-controller.js
@@ -1,3 +1,4 @@
+import multer from "multer"
 import { pool } from "../config/db.js"
 
 export const getAllGallery = async (_req, res) => {
@@ -6,5 +7,29 @@ export const getAllGallery = async (_req, res) => {
         res.status(200).json(result.rows)
     } catch (error) {
         res.status(500).json({ msg: error.message })
+    }
+}
+
+const storage = multer.memoryStorage();
+export const upload = multer({ storage });
+
+export const addImage = async (req, res) => {
+    try {
+        const { originalname, mimetype, buffer } = req.file;
+        const { description } = req.body;
+        const base64Image = buffer.toString("base64");
+
+        const query = `INSERT INTO images (filename, mimetype, image_base64, description) VALUES ($1, $2, $3, $4) RETURNING *`;
+        const values = [originalname, mimetype, base64Image, description];
+
+        const result = await pool.query(query, values);
+        res.json({
+            success: true,
+            message: "Gambar berhasil diunggah!",
+            data: result.rows[0],
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Upload gagal!" });
     }
 }
