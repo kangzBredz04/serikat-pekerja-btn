25d434252c3a167e1b59333a45691cf69edc5307
Wahyu <wabredz1234@gmail.com>
2025-02-28 10:40:53 +0700

    update gallery route

--- backend/api/routes/gallery-route.js
+++ backend/api/routes/gallery-route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { addImage, getImages, upload } from "../controllers/gallery-controller.js";
+import { addOrUpdateImage, getImages, upload } from "../controllers/gallery-controller.js";
 
 const router = express.Router();
 
 router.get("/get-all", getImages);
-router.post("/add-image", upload.single("image"), addImage);
+router.post("/add-image", upload.single("image"), addOrUpdateImage);
+router.put("/update-image", upload.single("image"), addOrUpdateImage);
 
 export default router;
