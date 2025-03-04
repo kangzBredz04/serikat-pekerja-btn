42493dc4f8a8dc2fce987d12df8f453d9e72a2b8
Wahyu <wabredz1234@gmail.com>
2025-03-04 11:48:26 +0700

    add route in auth route for add user by admin

--- backend/api/routes/auth-route.js
+++ backend/api/routes/auth-route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { deleteAccount, getAllAccount, loginAccount, logoutAccount } from "../controllers/auth-controller.js";
+import { addAccount, deleteAccount, getAllAccount, loginAccount, logoutAccount } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
 router.post("/login", loginAccount);
+router.post("/add", addAccount);
 router.get("/logout", logoutAccount);
 router.get("/get-all", getAllAccount);
 router.delete("/delete/:id", deleteAccount);
