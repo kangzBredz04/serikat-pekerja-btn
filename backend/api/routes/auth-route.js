2120cc27dd513b367738e867057431d8251b6183
Wahyu <wabredz1234@gmail.com>
2025-03-04 09:44:14 +0700

    add route for delete user in auth route

--- backend/api/routes/auth-route.js
+++ backend/api/routes/auth-route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getAllAccount, loginAccount, logoutAccount } from "../controllers/auth-controller.js";
+import { deleteAccount, getAllAccount, loginAccount, logoutAccount } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
 router.post("/login", loginAccount);
 router.get("/logout", logoutAccount);
 router.get("/get-all", getAllAccount);
+router.delete("/delete/:id", deleteAccount);
 
 export default router;
