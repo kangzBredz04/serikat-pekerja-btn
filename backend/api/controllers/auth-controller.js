d1e4bd04dfb954dcd3757f2ad001676f63a431da
Wahyu <wabredz1234@gmail.com>
2025-03-04 17:26:49 +0700

    commit

--- backend/api/controllers/auth-controller.js
+++ backend/api/controllers/auth-controller.js
@@ -2,6 +2,40 @@ import { pool } from "../config/db.js"
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
 
+// TANPA HASHING PASS
+// export const loginAccount = async (req, res) => {
+//     const { username, password } = req.body;
+//     try {
+//         // Mencari data user berdasarkan username
+//         const result = await pool.query(
+//             "SELECT * FROM users WHERE username = $1",
+//             [username]
+//         );
+//         if (result.rows.length > 0) {
+//             // Bandingkan password secara langsung
+//             if (result.rows[0].password === password) {
+//                 const token = jwt.sign(result.rows[0], process.env.SECRET_KEY);
+//                 // Set cookie
+//                 res.cookie("token", token, {
+//                     httpOnly: true,
+//                 });
+//                 res.status(200).json({
+//                     data: result.rows[0],
+//                     token,
+//                     msg: "Login berhasil !!!",
+//                 });
+//             } else {
+//                 return res.status(401).json({ msg: "Password salah !!!" });
+//             }
+//         } else {
+//             return res.status(404).json({ msg: "User tidak ditemukan !!!" });
+//         }
+//     } catch (error) {
+//         res.status(500).json({ error });
+//     }
+// };
+
+// DENGAN HASHING PASS
 export const loginAccount = async (req, res) => {
     const { username, password } = req.body;
     try {
@@ -10,9 +44,13 @@ export const loginAccount = async (req, res) => {
             "SELECT * FROM users WHERE username = $1",
             [username]
         );
-        if (result.rows.length > 0) {
-            // Bandingkan password secara langsung
-            if (result.rows[0].password === password) {
+        if (result.rows[0]) {
+            const isPasswordValid = await argon2.verify(
+                result.rows[0].password,
+                password
+            );
+
+            if (isPasswordValid) {
                 const token = jwt.sign(result.rows[0], process.env.SECRET_KEY);
                 // Set cookie
                 res.cookie("token", token, {
@@ -33,7 +71,6 @@ export const loginAccount = async (req, res) => {
         res.status(500).json({ error });
     }
 };
-
 export const logoutAccount = async (req, res) => {
     res.clearCookie("token");
     res.status(200).json({ msg: "Logout berhasil !!!" });
